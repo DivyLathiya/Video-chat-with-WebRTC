631ec6b9a9118007c0c908620821a10e61f1ce35
Jash Ambaliya <87864439+AJ0070@users.noreply.github.com>
2025-04-22 14:03:07 +0530

    Changed Port

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
